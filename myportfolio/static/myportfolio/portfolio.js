954953f78723a2465883a4e6557848685836b655
motokishibata <33312758+motokishibata@users.noreply.github.com>
2019-06-16 12:57:08 +0900

    CSSをbulma化/ポートフォリオ

--- myportfolio/static/myportfolio/portfolio.js
+++ myportfolio/static/myportfolio/portfolio.js
@@ -2,18 +2,15 @@ $(function(){
 
     // モーダルウィンドウが開くときの処理    
     $(".modalOpen").click(function(){
-        var navClass = $(this).attr("class"),
-            href = $(this).attr("href");
+        var href = $(this).attr("href");
                 
-            $(href).fadeIn();
-        $(this).addClass("open");
+        $(href).addClass("is-active");
         return false;
     });
      
     // モーダルウィンドウが閉じるときの処理    
-    $(".modalClose").click(function(){
-        $(this).parents(".modal").fadeOut();
-        $(".modalOpen").removeClass("open");
+    $(".modal-close").click(function(){
+        $(this).parents(".modal").removeClass("is-active");
         return false;
     });  
         
